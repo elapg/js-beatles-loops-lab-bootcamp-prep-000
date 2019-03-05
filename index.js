33f5668df59b3c3c24ed352da2c303576256cc63
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-05 02:20:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ function theBeatlesPlay(musicans,instruments){
   var array = [] ;
   for (let i = 0; i < musicans.length; i++)
 {
-  var string = '${musicans[i]'} plays ${instrument
+  var string = '${musicans[i]'} plays ${instrument[i]}';
+  array.push(string);
+}
+return array 
+}
+function johnLennonFacts(facts){
+  var array = [];
+  let i = 0;
+  while (i < facts.length){
+    array.push('${facts[i]}')
+  }
 }
