18338c22362531575611eb0bcff24d0077088c21
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-05 02:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) 
+  {
+    var string = `${musicians[i]} plays ${instruments[i]}`;
+    array.push(string);
+  }
+  return array
+}
 
-function theBeatlesPlay(musicans,instruments){
-  var array = [] ;
-  for (let i = 0; i < musicans.length; i++)
-{
-  var string = '${musicans[i]'} plays ${instrument[i]}';
-  array.push(string);
-}
-return array 
-}
-function johnLennonFacts(facts){
+function johnLennonFacts(facts) {
   var array = [];
   let i = 0;
-  while (i < facts.length){
-    array.push('${facts[i]}')
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
   }
+  return array
 }
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return array
